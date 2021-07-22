59507a68caf02d7786ab71564af959a073dff3c4
wangxuchuan <wangxuchuan@youzan.com>
2021-07-22 16:10:40 +0800

    first commit

---
+++ src/main/java/com/youzan/koderover/demo/KoderoverDemoApplication.java
@@ -0,0 +1,13 @@
+package com.youzan.koderover.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoderoverDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoderoverDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youzan/koderover/demo/KoderoverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youzan.koderover.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoderoverDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
